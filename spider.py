40697edfd1196b41994da8ea2ec2aa18d7668a39
Lemonh3add <135203670+Lemonh3add@users.noreply.github.com>
2024-09-19 17:16:22 -0500

    awd

---
+++ spider.py
@@ -0,0 +1,10 @@
+card_list = ["4H", "AS", "JD", "2C", "10H"]
+
+for i in range(len(card_list)):
+    print(card_list[i][0])
+
+print(card_list[0][0]) #1
+print(card_list[1][0]) #2
+print(card_list[2][0]) #3
+print(card_list[3][0]) #4
+print(card_list[4][0]) #5
